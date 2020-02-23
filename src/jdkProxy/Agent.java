41a02cf439932f82925b206e06edced9db999083
shihangqi <183521065@qq.com>
2020-02-23 10:21:02 +0800

    化简之后的代理类，更加简洁，使用了lambda表达式

--- src/jdkProxy/Agent.java
+++ src/jdkProxy/Agent.java
@@ -5,34 +5,47 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 public class Agent {
+//
+//    static class JdkProxySubject implements InvocationHandler {
+//
+//        private Object object;
+//
+//        public JdkProxySubject(Object object) {
+//            this.object = object;
+//        }
+//
+//        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//
+//            System.out.println("before");
+//            try {
+//                method.invoke(this.object, args);
+//            } catch (Throwable e) {
+//                e.printStackTrace();
+//            } finally {
+//                System.out.println("after");
+//            }
+//
+//            return null;
+//        }
+//    }
 
-    static class JdkProxySubject implements InvocationHandler {
 
-        private Object object;
+    public static Object agent(Object proxy) {
+        ClassLoader classLoader = proxy.getClass().getClassLoader();
+        Class<?>[] interfaces = proxy.getClass().getInterfaces();
+        return Proxy.newProxyInstance(classLoader, interfaces,
+                (object,method,args)->{
+                    System.out.println("before");
+                    try {
+                        method.invoke(proxy, args);
+                    } catch (Throwable e) {
+                        e.printStackTrace();
+                    } finally {
+                        System.out.println("after");
+                    }
 
-        public JdkProxySubject(Object object) {
-            this.object = object;
-        }
-
-        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-
-            System.out.println("before");
-            try {
-                method.invoke(this.object, args);
-            } catch (Throwable e) {
-                e.printStackTrace();
-            } finally {
-                System.out.println("after");
-            }
-
-            return null;
-        }
-    }
-
-
-    public static Object agent(Class interfaceClazz, Object proxy) {
-        return Proxy.newProxyInstance(interfaceClazz.getClassLoader(), new Class[]{interfaceClazz},
-                new JdkProxySubject(proxy));
+                    return null;
+                });
     }
 
 }
--- src/jdkProxy/ProxyTest.java
+++ src/jdkProxy/ProxyTest.java
@@ -7,10 +7,10 @@ public class ProxyTest {
 
     public static void main(String[] args) throws Exception {
 
-        SimpleIOC simpleIOC = new SimpleIOC("F:\\JavaWorkSpace\\Spring_IOC&AOP\\src\\simple.ioc\\simple.ioc.xml");
+        SimpleIOC simpleIOC = new SimpleIOC("F:\\JavaWorkSpace\\Spring_IOC&AOP\\src\\simple\\ioc\\ioc.xml");
         User user = (User) simpleIOC.getBean("user");
 
-        Subject subject = (Subject)Agent.agent(Subject.class,user);
+        Subject subject = (Subject)Agent.agent(user);
         subject.show();
     }
 }
